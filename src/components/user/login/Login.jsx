35db781903f9361e286177a75fb05ad0fcf83507
Alwyn Mathew <alwynmathew007@gmail.com>
2025-02-13 02:23:00 +0530

    register on login page

--- src/components/user/login/Login.jsx
+++ src/components/user/login/Login.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import loginImg from '../../../assets/login2.png'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Register from './Register';
+
+
 
 function Login() {
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
     return (
-        <div className='md:flex items-center md:mt-10'>
-            <div className=' md:ml-75'>
-                <p className='text-2xl text-center'>Login Here</p>
-                <div className='flex flex-col p-5 w-100%'>
-                    <TextField className='md:w-80' sx={{ marginBottom: '1rem'}} label="User Name" variant="standard" />
-                    <TextField className='md:w-80' sx={{ marginBottom: '1.5rem'}}  label="Password" variant="standard" />
-                    <Button variant="contained">Login</Button>
+        <>
+            <div className='md:flex items-center md:mt-10'>
+                <div className=' md:ml-75'>
+                    <p className='text-2xl text-center'>Login Here</p>
+                    <div className='flex flex-col p-5 w-100%'>
+                        <TextField className='md:w-80' sx={{ marginBottom: '1rem' }} label="User Name" variant="standard" />
+                        <TextField className='md:w-80' sx={{ marginBottom: '1.5rem' }} label="Password" variant="standard" />
+                        <Button variant="contained">Login</Button>
+                    </div>
+                </div>
+                <div className='flex justify-center pt-2'>
+                    <img src={loginImg} alt="reading" className='w-[360px] md:h-[200px]' />
                 </div>
             </div>
-            <div className='flex justify-center pt-2'>
-                <img src={loginImg} alt="reading" className='w-[360px] md:h-[200px]' />
-            </div>
-        </div>
+            <Button variant="text" onClick={handleClickOpen}>
+                Click to register here
+            </Button>
+            {open && <Register open={open} onClose={handleClose }/>}
+
+        </>
     )
 }
 
