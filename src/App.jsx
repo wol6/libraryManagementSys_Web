daa071fc958832281a1279fd4dadf0b9575c6c2a
Alwyn Mathew <alwynmathew007@gmail.com>
2025-02-25 22:43:22 +0530

    routes and wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Routes,Route } from 'react-router-dom'
+
 import Admin from './components/admin/admin'
 import Login from './components/user/login/Login'
+import Home from './components/user/Home'
 
 function App() {
 
   return (
     <>
       {/* <Admin /> */}
-      <Login/>
+      {/*  */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </>
   )
 }
---
+++ src/components/user/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../layouts/Footer'
+import Navigation from '../layouts/Navigation'
+
+function Home() {
+  return (
+    <div>
+        <Navigation/>
+        <h1>Home</h1>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
