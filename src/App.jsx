00dcab9113db78027ab2b88780f1f4428d82553f
Alwyn Mathew <alwynmathew007@gmail.com>
2025-03-01 00:46:39 +0530

    wip nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import { Routes,Route } from 'react-router-dom'
 import Admin from './components/admin/admin'
 import Login from './components/user/login/Login'
 import Home from './components/user/Home'
+import Navigation from './components/layouts/Navigation'
 
 function App() {
 
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route path='/admin' element={<Admin />}/>
         <Route path='/login/:admin' element={<Login/>}/>
 
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
-      </Routes>
+      </Routes> */}
+      <Navigation/>
     </>
   )
 }
--- src/components/layouts/Navigation.jsx
+++ src/components/layouts/Navigation.jsx
@@ -3,11 +3,23 @@ import { NavLink } from 'react-router-dom'
 
 function Navigation() {
   return (
-    <div>
-      <nav>
+    <div className='border-2 flex justify-around mt-4 p-4'>
+      {/* <nav>
         <NavLink to='/'>Home</NavLink>
         <NavLink to='/login'>Login</NavLink>
         <NavLink to='/admin'>Admin</NavLink>
+      </nav> */}
+      <p>logo</p>
+      <nav>
+        <a href="#">Library</a>
+        <a href="#">About</a>
+      </nav>
+      <div>
+        <input type="text" className='border-2' placeholder='search' />
+        <button>search</button>
+      </div>
+      <nav>
+        <a href="#">login</a>
       </nav>
     </div>
   )
