1c8db86d11aabb16432e838611d54940bddf1aee
Alwyn Mathew <alwynmathew007@gmail.com>
2025-03-08 21:19:00 +0530

    img link

--- src/components/user/Home.jsx
+++ src/components/user/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
         </div>
 
         <div className='border-2 w-45 ml-12'>
-          <img src={book} alt="" />
+          <img src='https://m.media-amazon.com/images/I/71HX66uNvfL._SY342_.jpg' alt="" />
           <button className='ml-12'>Borrow</button>
         </div>
 
