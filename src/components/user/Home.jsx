a9f2fe26c725dc12b5cd1b9491bba7b8a44da6b3
Alwyn Mathew <alwynmathew007@gmail.com>
2025-04-11 23:32:35 +0530

    wip:get books ui

--- src/components/user/Home.jsx
+++ src/components/user/Home.jsx
@@ -1,49 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import bookImg from '../../assets/book_test.jpg'
+import axios from 'axios'
 
-const books = [
-  {
-    id: 1,
-    title: 'The Richest Man in Babylon',
-    author: 'George S. Clason',
-    image: bookImg,
-  },
-  {
-    id: 2,
-    title: 'The Richest Man in Babylon',
-    author: 'George S. Clason',
-    image: 'https://m.media-amazon.com/images/I/71HX66uNvfL._SY342_.jpg',
-  },
-];
+// const books = [
+//   {
+//     id: 1,
+//     title: 'The Richest Man in Babylon',
+//     author: 'George S. Clason',
+//     image: bookImg,
+//   },
+//   {
+//     id: 2,
+//     title: 'The Richest Man in Babylon',
+//     author: 'George S. Clason',
+//     image: 'https://m.media-amazon.com/images/I/71HX66uNvfL._SY342_.jpg',
+//   },
+// ];
 
 function Home() {
+  const [books, setBooks] = useState([])
+
+  async function getAllBooks() {
+    try {
+      const { data: resp } = await axios.get('http://localhost:5000/getbook')
+      console.log(resp)
+      if (resp.success) {
+        setBooks(resp.books)
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    getAllBooks()
+  }, [])
   return (
-  <>
-  <h1 className='text-center text-2xl'>Library</h1>
-    <div className='flex'>
-        {/* <div className='border-2 w-45 ml-12'>
-          <img src={book} alt="" />
-          <button className='ml-12'>Borrow</button>
-        </div>
-
-        <div className='border-2 w-45 ml-12'>
-          <img src='https://m.media-amazon.com/images/I/71HX66uNvfL._SY342_.jpg' alt="" />
-          <button className='ml-12'>Borrow</button>
-        </div> */}
-
-        {books.map((book)=>{
-          return(
-           <div className='border-0 w-60 ml-12' key={book.id}>
-           <img src={book.image} alt="" />
-           <p className='text-center'>{book.title}</p>
-           <p className='text-center'>{book.author}</p>
-           <button className='ml-20'>Borrow</button>
-         </div>
+    <>
+      <h1 className='text-center text-2xl'>Library</h1>
+      <div className='flex border-2 '>
+        {books.map((book) => {
+          return (
+            <div className='w-60 ml-12' key={book._id}>
+              <img src={book.imgurl} alt="" />
+              <p className='text-center'>{book.bookname}</p>
+              <p className='text-center'>{book.author}</p>
+              <button className='ml-20'>Borrow</button>
+            </div>
           )
         })}
 
-    </div>
-  </>
+      </div>
+    </>
   )
 }
 
