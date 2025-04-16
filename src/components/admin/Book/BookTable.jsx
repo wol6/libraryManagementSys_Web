f82831b78bb898922410f4c59fcdb122d8c83394
Alwyn Mathew <alwynmathew007@gmail.com>
2025-04-16 22:46:26 +0530

    live user and book table

--- src/components/admin/Book/BookTable.jsx
+++ src/components/admin/Book/BookTable.jsx
@@ -1,48 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
+import axios from 'axios';
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
+    // { field: '_id', headerName: 'ID', width: 70 },
+    { field: 'bookname', headerName: 'Book Name', width: 130 },
+    { field: 'author', headerName: 'Author', width: 130 },
+    { field: 'imgurl', headerName: 'Image url', width: 130 },
+    { field: 'availabilityStatus', headerName: 'Availability Status', width: 130 },
     {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-    },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
+        field: 'edit',
+        headerName: 'Action',
+        description: 'To edit the field',
         sortable: false,
-        width: 160,
-        valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+        width: 130
     },
-];
-
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
 const paginationModel = { page: 0, pageSize: 5 };
 
 function BookTable() {
+    const [rows, setRows] = useState([])
+
+    useEffect(() => {
+        getAllBooks()
+    }, [])
+
+    async function getAllBooks() {
+        try {
+            const { data: resp } = await axios.get('http://localhost:5000/getbook')
+            if (resp.success) {
+                setRows(resp.books)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
     return (
         <div className='ml-75'>
             <Paper sx={{ height: 400, width: '65%' }}>
-                <DataGrid 
+                <DataGrid
                     rows={rows}
                     columns={columns}
+                    getRowId={(row) => row._id}
                     initialState={{ pagination: { paginationModel } }}
                     pageSizeOptions={[5, 10]}
                     // checkboxSelection
