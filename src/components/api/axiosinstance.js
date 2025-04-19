b98dc709f10f4dacecb58f03e4fe9e84d057a9fa
Alwyn Mathew <alwynmathew007@gmail.com>
2025-04-19 23:33:09 +0530

    instance

---
+++ src/components/api/axiosinstance.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const Ax = axios.create({
+    baseURL: 'http://localhost:5000/api', //add base url
+    timeout: 5000
+})
+
+Ax.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');//enter token key
+        console.log('test token',token)
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+Ax.interceptors.response.use((response) => {
+    return response
+},
+    (error) => {
+        const { response } = error;
+        console.log('Unauthorized! Redirecting to login...', response);
+        // window.location.href = '/login';
+        return Promise.reject(error);
+    }
+)
+
+export default Ax
