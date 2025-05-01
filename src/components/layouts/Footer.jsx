418fdf061e62d42be7e313187c05f397e5db5aba
Alwyn Mathew <alwynmathew007@gmail.com>
2025-05-02 02:17:14 +0530

    year

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 function Footer() {
+  const dt = new Date()
+  const year = dt.getFullYear()
   return (
 <div>
 <footer className="bg-primary p-4 text-center bottom-0 left-0 w-full">
-    <p className="text-cyan-800">© 2025 My Website. All rights reserved.</p>
+    <p className="text-cyan-800">© {year} Reader Website. All rights reserved.</p>
   </footer>
 </div>
   )
